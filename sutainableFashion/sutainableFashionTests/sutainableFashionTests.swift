b1a4c50535b9ebe332cc6e182f2f883969db09fa
theunghee02 <theunghee02@gmail.com>
2025-05-17 15:47:31 +0900

    feat: initial setting

---
+++ sutainableFashion/sutainableFashionTests/sutainableFashionTests.swift
@@ -0,0 +1,17 @@
+//
+//  sutainableFashionTests.swift
+//  sutainableFashionTests
+//
+//  Created by 최승희 on 5/17/25.
+//
+
+import Testing
+@testable import sutainableFashion
+
+struct sutainableFashionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
